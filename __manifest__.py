cf91dae700e8d43d8c496c01027b5e9b5ea2cbf6
Christopher Ormaza <chris.ormaza@gmail.com>
2021-05-06 15:21:45 -0500

    archivos del proyecto

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Transmedical Customization",
+    "version": "13.0.0.0.1",
+    "summary": "Transmedical Customization",
+    "description": "Transmedical Customization",
+    "category": "Partners",
+    "author": "PagarEsFacil, Christopher Ormaza",
+    "website": "https://planproecuador.com",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+        "l10n_ec_niif",
+        "muk_rest",
+        "contract",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/external_invoice_view.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "external_dependencies": {
+        "python": [],
+    },
+}
