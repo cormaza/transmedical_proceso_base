e92e5b98fb2bc2cdb2a769296a5c094903aabc53
Christopher Ormaza <chris.ormaza@gmail.com>
2021-10-18 07:21:31 -0500

    IMP: agregado de estructura para diagnosticos y procedimientos, edad, sexo al que aplica

--- models/medical_diagnostic.py
+++ models/medical_diagnostic.py
@@ -4,51 +4,54 @@ from odoo.osv import expression
 
 class MedicalDiagnostic(models.Model):
 
-    _name = 'medical.diagnostic'
-    _description = 'Medical Diagnostic'
+    _name = "medical.diagnostic"
+    _description = "Medical Diagnostic"
     _inherit = [
         "mail.thread",
         "mail.activity.mixin",
     ]
 
     active = fields.Boolean(
-        string='Active',
+        string="Active",
         required=False,
         default=True,
         tracking=True,
     )
     code = fields.Char(
-        string='Diagnostic Code',
+        string="Diagnostic Code",
         required=True,
         tracking=True,
     )
     name = fields.Char(
-        string='Diagnostic Name',
+        string="Diagnostic Name",
         required=True,
-        tracking = True,
+        tracking=True,
     )
+    sex = fields.Selection(
+        string="Sex",
+        selection=[("male", "Male"), ("female", "Female"), ("any", "Any")],
+        default="any",
+        required=False,
+    )
+    min_age = fields.Integer(string="Min Age", required=False)
+    max_age = fields.Integer(string="Max Age", required=False)
 
-    _sql_constraints = [
-        ("code_uniq", "unique(code)", "Medical diagnostic must have unique code")
-                        ]
-
-
+    _sql_constraints = [("code_uniq", "unique(code)", "Medical diagnostic must have unique code")]
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         args = args or []
         domain = []
         if name:
-            domain = ['|', ('code', '=ilike', name + '%'), ('name', operator, name)]
+            domain = ["|", ("code", "=ilike", name + "%"), ("name", operator, name)]
             if operator in expression.NEGATIVE_TERM_OPERATORS:
-                domain = ['&'] + domain
+                domain = ["&"] + domain
         recs = self.search(domain + args, limit=limit)
         return recs.name_get()
 
     def name_get(self):
         result = []
         for r in self:
-            name = '%s - %s' % (r.code, r.name)
+            name = "{} - {}".format(r.code, r.name)
             result.append((r.id, name))
         return result
-
--- models/medical_procedure.py
+++ models/medical_procedure.py
@@ -4,51 +4,46 @@ from odoo.osv import expression
 
 class MedicalProcedure(models.Model):
 
-    _name = 'medical.procedure'
-    _description = 'Medical Procedure'
+    _name = "medical.procedure"
+    _description = "Medical Procedure"
     _inherit = [
         "mail.thread",
         "mail.activity.mixin",
     ]
 
     active = fields.Boolean(
-        string='Active',
+        string="Active",
         required=False,
         default=True,
         tracking=True,
     )
-    code = fields.Char(
-        string='Procedure Code',
-        required=True,
-        tracking=True
+    code = fields.Char(string="Procedure Code", required=True, tracking=True)
+    name = fields.Char(string="Procedure Name", required=True, tracking=True)
+    sex = fields.Selection(
+        string="Sex",
+        selection=[("male", "Male"), ("female", "Female"), ("any", "Any")],
+        default="any",
+        required=False,
     )
-    name = fields.Char(
-        string='Procedure Name',
-        required=True,
-        tracking=True
-    )
+    min_age = fields.Integer(string="Min Age", required=False)
+    max_age = fields.Integer(string="Max Age", required=False)
 
-    _sql_constraints = [
-        ("code_uniq", "unique(code)", "Medical procedure must have unique code")
-                        ]
-
-
+    _sql_constraints = [("code_uniq", "unique(code)", "Medical procedure must have unique code")]
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         args = args or []
         domain = []
         if name:
-            domain = ['|', ('code', '=ilike', name + '%'), ('name', operator, name)]
+            domain = ["|", ("code", "=ilike", name + "%"), ("name", operator, name)]
             if operator in expression.NEGATIVE_TERM_OPERATORS:
-                domain = ['&'] + domain
+                domain = ["&"] + domain
         recs = self.search(domain + args, limit=limit)
         return recs.name_get()
 
     def name_get(self):
         result = []
         for r in self:
-            name = '%s - %s' % (r.code, r.name)
+            name = "{} - {}".format(r.code, r.name)
             result.append((r.id, name))
         return result
-
