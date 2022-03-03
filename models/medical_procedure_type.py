e929074f0b4b841d5f0fabd24e0117a435eb2fa4
nelio2120 <48095432+nelio2120@users.noreply.github.com>
2022-03-03 09:26:48 -0500

    [ADD] Changes in the ode for the rates by provider (#12)
    * [ADD] Changes in the ode for the rates by provider
    
    * [FIX] replace function display name for name get for standarization

--- models/medical_procedure_type.py
+++ models/medical_procedure_type.py
@@ -5,31 +5,31 @@ from odoo.exceptions import UserError
 class MedicalProcedureType(models.Model):
     _name = 'medical.procedure.type'
     _description = 'Medical Type Procedure'
-    _rec_name = "display_name"
 
     code = fields.Char("Code")
     name = fields.Char("Name")
-    display_name = fields.Char("Display Name", compute="_get_name_get")
     supplier_id = fields.Many2one(
         comodel_name='res.partner',
         string='Supplier',
         domain=[('medical_service_provider', '=', True)]
     )
 
-    @api.depends("code", "supplier_id")
-    def _get_name_get(self):
-        for record in self:
-            record.display_name = "{0} - {1}".format(
-                record.name,
-                record.supplier.name,
+    def name_get(self):
+        result = []
+        for rec in self:
+            name = "{} - {}".format(
+                rec.name,
+                rec.supplier_id.name,
             )
+            result.append((rec.id, name))
+        return result
 
     @api.constrains("code", "supplier_id")
     def _constraint_code(self):
         for record in self:
             if self.env['medical.procedure.type'].search([
                 ('code', '=', record.code),
-                ('supplier_id', '=', record.supplier.id),
+                ('supplier_id', '=', record.supplier_id.id),
                 ('id', '!=', record.id)
             ]):
                 raise UserError(_("Medical procedure type must have unique code"))
